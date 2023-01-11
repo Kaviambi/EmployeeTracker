0d4aa96d64f0998d8faba0e70bfe3c8e2e89783e
kavic <kavicse.23@gmail.com>
2023-01-11 00:16:37 -0600

    Working on adding roles,employee

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+/* database */
+INSERT INTO department(dept_name)
+VALUES ("engineering"), 
+("finance"),
+("legal"),
+("sales");
+
+INSERT INTO employee_role (title, salary, department_id)
+VALUES ("Sales Lead", 100000,1),
+("Sales Person", 70000,2),
+("Lead engineering", 90000,3),
+("Software Engineering", 100000,4),
+("Lawyer", 2000,2),
+("Account Manager", 500000,1);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("April", "Neil", 5, 1),
+("Dani", "Gary", 6, 2),
+("Jess", "Gonzalues", 1, 4),
+("Ravi", "Jones", 3, 6),
+("Siva", "Mathews", 4, 5),
+("Kevin", "Chris", 1, 4);
